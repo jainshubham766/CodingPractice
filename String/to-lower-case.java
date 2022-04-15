3111d6ec08f3a42e1ff21c7a997d5d141d023674
Shubham Jain <jainshubham766@gmail.com>
2022-04-15 23:16:34 +0530

    Important Fundamental Approach

---
+++ String/to-lower-case.java
@@ -0,0 +1,17 @@
+Question: https://leetcode.com/problems/to-lower-case/
+
+
+class Solution {
+    public String toLowerCase(String s) {
+
+
+        // Approach 01: Brute Force:
+
+        return s.toLowerCase();
+
+        //Approach 02: Using ASCII values and ADD accordingly
+
+        // if charcter is between 65-90 add +32 in it
+
+    }
+}
